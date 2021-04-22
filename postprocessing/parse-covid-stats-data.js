98d29b258fc957c90310a466c1ad1d400e2bdef8
Amelia Wattenbeger <wattenberger@github.com>
2021-04-22 15:22:10 -0400

    dont delete covid stats csv

--- postprocessing/parse-covid-stats-data.js
+++ postprocessing/parse-covid-stats-data.js
@@ -10,6 +10,6 @@ const outputFilename = inputFilename.replace(".csv", ".json");
 const contents = await readCSV(inputFilename, { skipFirstRow: true });
 const parsedContents = contents.map((d) => ({ ...d, Date: String(d["Date"]) }));
 
-await removeFile(inputFilename);
+// await removeFile(inputFilename);
 
 await writeJSON(outputFilename, parsedContents);
