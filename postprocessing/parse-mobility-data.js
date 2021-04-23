4208d649e4b1e20e78ee18a39197e7629793567b
Amelia Wattenbeger <wattenberger@github.com>
2021-04-23 16:03:29 -0400

    fix date parsing

--- postprocessing/parse-mobility-data.js
+++ postprocessing/parse-mobility-data.js
@@ -3,7 +3,7 @@ import {
   writeJSON,
   removeFile,
 } from "https://deno.land/x/flat@0.0.5/mod.ts";
-import { parse, format } from "https://deno.land/std@0.69.0/datetime/mod.ts";
+import { format } from "https://deno.land/std@0.69.0/datetime/mod.ts";
 
 const inputFilename = Deno.args[0];
 const outputFilename = inputFilename.replace(".csv", ".json");
@@ -12,7 +12,8 @@ const contents = await readCSV(inputFilename, { skipFirstRow: true });
 const parsedContents = contents
   .filter((d) => d["sub_region_1"] !== "sub_region_1")
   .map((d) => ({
-    date: format(new Date(d["date"]), "MM/dd/yyyy"),
+    ...d,
+    date: format(new Date(+d["date"]), "MM/dd/yyyy"),
   }));
 
 await writeJSON(outputFilename, parsedContents);
